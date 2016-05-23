bb5529a7c4a150dd24eeaf27e9dc5b54db69faef
Marcin Okon <Marcin Okon>
2016-05-23 21:26:10 +0200

    Init

---
+++ PicasaXmlProvider.cs
@@ -0,0 +1,21 @@
+﻿using GoogleAuthentication;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace GooglePhotosUploader.Code.ImageHoster.Picasa
+{
+    class PicasaXmlProvider
+    {
+        public static IEnumerable<XElement> GetEntries(string url, string username)
+        {
+            var response = PicasaConnection.GetWebResponse(url, username);
+            if (response != null)
+            {
+                var xml = XDocument.Parse(response);
+                return xml.Root.Elements(XmlNamespaces.Atom + "entry");
+            }
+            return Enumerable.Empty<XElement>();
+        }
+    }
+}
---
+++ RequestManager.cs
@@ -0,0 +1,23 @@
+﻿using GooglePhotosUploader.Code.ImageHoster.Picasa;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace PicasaAPI
+{
+    public class RequestManager
+    {
+        public static IEnumerable<XElement> GetAlbums(string username)
+        {
+            var userUrl = "https://picasaweb.google.com/data/feed/api/user/default";
+            return PicasaXmlProvider.GetEntries(userUrl, username);
+        }
+
+        public static IEnumerable<XElement> GetMedia(string username, string albumId)
+        {
+            var albumUrl = string.Format("https://picasaweb.google.com/data/feed/api/user/default/albumid/{0}?imgmax=1024", albumId);
+            return PicasaXmlProvider.GetEntries(albumUrl, username);
+        }
+    }
+}
