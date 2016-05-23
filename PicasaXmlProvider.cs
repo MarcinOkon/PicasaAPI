1fbf62d31ed187db160b27cb9b0284c6feaf70aa
Marcin Okon <Marcin Okon>
2016-05-23 22:20:59 +0200

    split

--- PicasaXmlProvider.cs
+++ PicasaXmlProvider.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Xml.Linq;
 
-namespace GooglePhotosUploader.Code.ImageHoster.Picasa
+namespace PicasaAPI
 {
     class PicasaXmlProvider
     {
---
+++ XmlNamespaces.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml.Linq;
+
+namespace PicasaAPI
+{
+    class XmlNamespaces
+    {
+        public static XNamespace Atom { get { return "http://www.w3.org/2005/Atom"; } }
+        public static XNamespace GPhoto { get { return "http://schemas.google.com/photos/2007"; } }
+        public static XNamespace Exif { get { return "http://schemas.google.com/photos/exif/2007"; } }
+        public static XNamespace Media { get { return "http://search.yahoo.com/mrss/"; } }
+    }
+}
